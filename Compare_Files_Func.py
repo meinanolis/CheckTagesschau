0278eab6cb0ba3c047ef662ddb76ba82a640f7a7
Jan Paschen <jan@meinanolis.de>
2018-12-15 00:17:38 +0100

    compare files func funktioniert

--- Compare_Files_Func.py
+++ Compare_Files_Func.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import difflib
 import MySQLdb
 import numpy as np
+import webbrowser
 
 connection = MySQLdb.connect(
      host="192.168.2.3",
@@ -16,11 +17,27 @@ connection = MySQLdb.connect(
 def Compare(path,name,version1,version2):
     textbausteine1=get_Textbausteine(path,name,version1)
     textbausteine2=get_Textbausteine(path,name,version2)
+    t1=[t.text for t in textbausteine1]
+    t2=[t.text for t in textbausteine2]
     idiff=0
-    for d in difflib.context_diff(textbausteine1,textbausteine2,n=0):
-        print(d)
+    for d in difflib.context_diff(t1,t2,n=0):
+        #print(d)
         idiff+=1
-    print(idiff)
+    print(idiff, name)
+    if idiff:
+        html = difflib.HtmlDiff().make_table(fromlines=t1,tolines=t2)
+        soup=BeautifulSoup(html)
+        #soup.find('span', class_='diff_chg')['color']='red'
+        soup.find('table')['width']='1000'
+        soup.find('table')['cellspacing']='10'
+        soup.find('table')['cellpadding']='10'
+        text=soup.prettify(formatter=lambda s: s.replace(u'\xa0', ' ')).replace('nowrap="nowrap"', 'width="450"')
+        text=text.replace('<html>','<html><head><style>span.diff_sub { background-color: red;}\nspan.diff_add {background-color:green;}\nspan.diff_chg {background-color:yellow}</style></head>')
+        with open(path+'/diff_'+str(version1)+'-'+str(version2)+'.html', 'w') as f:
+            f.write(text)
+        #webbrowser.open(url)
+        
+    return idiff
 
 def get_Textbausteine(path,name,version):
     f=open(path+'/'+str(version)+'_'+name)
@@ -28,23 +45,25 @@ def get_Textbausteine(path,name,version):
     f.close()
     soupe=BeautifulSoup(sauce, 'lxml')
     body=soupe.body
-    stand=body.find('span', class_='stand').text
     content=body.find(id='content')
-    section=content.find('div',class_='sectionZ')
-    ssection=section.find('div',class_='modParagraph')
-    textbausteine=ssection.find_all('p',class_='text')
-    h2s=content.find_all('h2', class_='subtitle')
-    head=soupe.head
-    browsertitle=head.title.text
-    headline=body.find('span', class_='headline').text
-    dachzeile=body.find('span', class_='dachzeile').text	
-    stand=body.find('span', class_='stand').text	
-    for i in range(len(textbausteine)):
-    	if textbausteine[i].find('a'):
-    		if '#comment-' in textbausteine[i].find('a')['href']:
-    			del textbausteine[i]
-    textbausteine=[t.text for t in textbausteine]+[h.text for h in h2s]+[browsertitle,headline,dachzeile,stand]
-    return textbausteine
+    contentlist=[]
+    h1=content.h1
+    contentlist.append(h1)
+    stand=content.find('span', class_='stand')
+    contentlist.append(stand)
+
+    section=content.find('div',class_='sectionZ').find('div',class_='modParagraph')
+    for child in section.contents:
+        if 'class="text small' in str(child):
+            contentlist.append(child)
+        elif '<h2 class="subtitle' in str(child):
+            contentlist.append(child)
+        elif 'image">' in str(child):
+            image=child.find('img')
+            contentlist.append(image)
+            infotext=child.find('p', class_="infotext")
+            contentlist.append(infotext)
+    return contentlist
 
 
 def sync_Uebersicht_und_Vergleich():
@@ -72,14 +91,26 @@ def sync_Uebersicht_und_Vergleich():
 
 
 
-path='2018-11/20-jahre-iss-101.html'
-name='20-jahre-iss-101.html'
-version1=0
-version2=1
+
 #sync_Uebersicht_und_Vergleich()
 c=connection.cursor()
 c.execute('SELECT ID FROM Vergleich WHERE `Vergleich`.`1-2` IS NULL')
 table_rows = c.fetchall()
 table_rows=list(table_rows)
-#Compare(path,name,version1,version2)
+version1=1
+version2=2
+for row in table_rows:
+    ID=row[0]
+    c.execute('SELECT `Name`,`RockPfad` FROM `Uebersicht` WHERE `ID`='+str(ID))
+    table_rows = c.fetchall()
+    table_rows=list(table_rows)
+    name, path = table_rows[0]
+    try:
+        idiff=Compare(path,name,version1,version2)
+    except:
+        idiff=999
+    c.execute('UPDATE `Vergleich` SET `2-3` = '+str(idiff)+' WHERE `Vergleich`.`ID` = '+str(ID)+';')
+    connection.commit()
+
+
 
--- test.py
+++ test.py
@@ -4,28 +4,37 @@ from bs4 import BeautifulSoup
 import difflib
 #import webbrowser
 import MySQLdb
+import sys
 
-os.chdir('//RASPBERRYPI/PiShare/CheckTagesschau/sharefolder/')
+f=open('2018-11/migranten-us-grenze-101.html/0_migranten-us-grenze-101.html')
+sauce=f.read()
+f.close()
+soupe=BeautifulSoup(sauce, 'lxml')
+body=soupe.body
+content=body.find(id='content')
+h1=content.h1
+stand=content.find('span', class_='stand')
+section=content.find('div',class_='sectionZ').find('div',class_='modParagraph')
 
+contentlist=[]
 
-class article:
-	def __init__(ID,name,pfad):
-		self.ID=ID
-		self.name=name
-		self.pfad=pfad
+contentlist.append(h1)
+contentlist.append(stand)
 
+for child in section.contents:
+	if 'class="text small' in str(child):
+		contentlist.append(child)
+	elif '<h2 class="subtitle' in str(child):
+		contentlist.append(child)
+	elif 'image">' in str(child):
+		image=child.find('img')
+		contentlist.append(image)
+		infotext=child.find('p', class_="infotext")
+		contentlist.append(infotext)
+print(len(contentlist))
 
-connection = MySQLdb.connect(
-     host="192.168.2.100",
-     port=3307,
-     db="Tagesschau",
-     user="Tagesschau", passwd="g00gle"
-    )
-
-c=connection.cursor()
-
-c.execute('SELECT `ID`,`Name`,`RockPfad` FROM `Uebersicht`')
-	table_rows = c.fetchall()
-	table_rows=list(table_rows)
-	for row in table_rows[1:]:
-		ID,name,pfad=row
+f=open('test2.html','wb')
+f.write(b'<html><body>')
+f.write(h1.prettify("utf-8"))
+f.write(b'</body></html>')
+f.close()

ef5d464fee3c5900669fcd34a74d8260f886d1fb
meinanolis <jan@meinanolis.de>
2019-01-06 20:09:14 +0100

    VERSION Hinzugefügt. Ungetestet

--- CompareVersions/Content_Frame.py
+++ CompareVersions/Content_Frame.py
@@ -30,13 +30,14 @@ def bin_vec_to_dict(bin_vec_int):
 
 
 #------------TODO    
-def write_bin_vec_to_db(ID, bin_vec_int):
+def write_bin_vec_to_db(ID, VERSION, bin_vec_int):
     return None
 
 #------------TODO  
-def read_data_from_db(ID):
+def read_data_from_db(ID, VERSION):
     data_dict = {
         'ID': ID,
+        'VERSION' : 1,
         'Name': 'name',
         'Path': 'path',
         'Date': 'date',
@@ -49,7 +50,8 @@ def read_data_from_db(ID):
 #------------TODO  
 def get_random_888():
     ID=111
-    return ID
+    VERSION=1
+    return ID, VERSION
 
 
 #------------TODO  kann das framing ding werden
@@ -57,17 +59,17 @@ def get_random_888():
 def hello():
     return "Hello World!"
 
-@app.route("/id/<int:ID>/")
-def outer_frame(ID):
-    data_dict = read_data_from_db(ID)
+@app.route("/id/<int:ID>/<int:VERSION>/")
+def outer_frame(ID,VERSION):
+    data_dict = read_data_from_db(ID,VERSION)
     d = bin_vec_to_dict(data_dict['bin_vec_int'])
     print(data_dict)
     return render_template('outer_frame.html',data_dict=data_dict, d=d)
 
 @app.route("/next/")
 def outer_frame_next():
-    ID = get_random_888()
-    data_dict = read_data_from_db(ID)
+    ID, VERSION = get_random_888()
+    data_dict = read_data_from_db(ID, VERSION)
     d = bin_vec_to_dict(data_dict['bin_vec_int'])
     print(data_dict)
     return render_template('outer_frame.html',data_dict=data_dict, d=d)
@@ -77,9 +79,10 @@ def result():
     if request.method == 'POST':
         result = request.form
         ID = result['ID']
+        VERSION = result['VERSION']
         bin_vec_int = dict_to_bin_vec(result)
-        write_bin_vec_to_db(ID, bin_vec_int)
-    return 'ok '+str(ID)+' '+str(bin_vec_int)
+        write_bin_vec_to_db(ID, VERSION, bin_vec_int)
+    return 'ok, ID: '+str(ID)+', Version: '+str(VERSION)+', Bin_Vec '+str(bin_vec_int)
 
 if __name__ == "__main__":
     app.run()
